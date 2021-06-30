374451b04c0535ce314c5fd2ea3ec5fff5e22557
mingkais <mingkais@goyu-ai.com>
2021-06-30 15:43:41 +0800

    测试代码

---
+++ src/main/java/com/kotei/test/DemoController.java
@@ -0,0 +1,28 @@
+package com.kotei.test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author sun.ming.kai
+ * @Date 2021-06-25
+ * @Description: TODO
+ */
+@RefreshScope
+@RestController
+@RequestMapping("/api/demo")
+class DemoController{
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+    @GetMapping("/getValue")
+    public String getValue(){
+        return "[username: " + username + ", password: " + password + "]";
+    }
+}
